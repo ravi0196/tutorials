760ebaa0254d1a5bccd6cfeb58b33e7da1cb434d
ravi0196 <raviteja.grandhi0112@gmail.com>
2025-04-23 06:10:52 +0530

    fixed syntax error in files.

--- terraform/6-routes.tf
+++ terraform/6-routes.tf
@@ -2,7 +2,7 @@ resource "aws_route_table" "private" {
   vpc_id=aws_vpc.main.id
   route{
     cidr_block="0.0.0.0/0"
-    nat_gateway_id=aws_nat_gateway.nat.id
+    nat_gateway_id =aws_nat_gateway.nat.id
 
   }
   tags={
@@ -14,7 +14,7 @@ resource "aws_route_table" "public" {
   vpc_id=aws_vpc.main.id
   route{
     cidr_block="0.0.0.0/0"
-    nat_gateway_id=aws_internet_gateway.igw.id
+    gateway_id =aws_internet_gateway.igw.id
   }
   tags={
     Name ="${local.env}-public"
@@ -22,21 +22,21 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "private_zone1" {
-  subnetid=aws_subnet.private_zone1.id
+  subnet_id=aws_subnet.private_zone1.id
   route_table_id=aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "private_zone2" {
-  subnetid=aws_subnet.private_zone2.id
+  subnet_id=aws_subnet.private_zone2.id
   route_table_id=aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "public_zone1" {
-  subnetid=aws_subnet.public_zone1.id
+  subnet_id=aws_subnet.public_zone1.id
   route_table_id=aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "public_zone2" {
-  subnetid=aws_subnet.public_zone2.id
+  subnet_id=aws_subnet.public_zone2.id
   route_table_id=aws_route_table.public.id
 }
